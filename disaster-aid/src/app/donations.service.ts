62577d120514ca224d6b3b70cab402af7aa48593
luisrampolla <luis.rampolla@upr.edu>
2020-05-09 18:49:10 -0400

    implement donation services to component

---
+++ disaster-aid/src/app/donations.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Donation } from './donation';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DonationsService {
+
+  // mock list. in reality it would get loaded with db donations (GET all donations)
+  public donationsList: Donation[] = [
+    {
+      did: 1,
+      supplyName: 'Water',
+      quantity: 10,
+      time: { hours: 12, minutes: 12 },
+      unit: '8oz bottles'
+    },
+    {
+      did: 2,
+      supplyName: 'Toilet Paper',
+      quantity: 5,
+      time: { hours: 12, minutes: 12 },
+      unit: 'Rolls'
+    },
+    {
+      did: 3,
+      supplyName: 'Granola Bars',
+      quantity: 8,
+      time: { hours: 12, minutes: 12 },
+      unit: 'Boxes of 12'
+    }
+  ]
+
+  constructor() { }
+
+  getDonations(): Donation[] {
+    return this.donationsList;
+  }
+
+  getDonationById(id: number): Donation {
+    // return donation based on id
+    let dummyDonationToReturn: Donation = {
+      did: 1,
+      supplyName: 'Water',
+      quantity: 10,
+      time: { hours: 12, minutes: 12 },
+      unit: '8oz bottles'
+    }
+    return dummyDonationToReturn;
+  }
+
+  addDonation(donation: Donation): void {
+    // here it is getting pushed to the array to be displayed but also needs to be pushed to db (POST new donation)
+    this.donationsList.push({
+      did: this.donationsList.length + 1,
+      supplyName: donation.supplyName,
+      quantity: donation.quantity,
+      time: { hours: 12, minutes: 12 },
+      unit: donation.unit
+    });
+  }
+
+  deleteDonation(id: number): void {
+
+  }
+
+  editDonation(donation: Donation): void {
+    // using donation id, update the other fields
+  }
+}
--- disaster-aid/src/app/donations/donations.component.ts
+++ disaster-aid/src/app/donations/donations.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Donation } from '../donation'
 import { FormBuilder } from '@angular/forms';
+import { DonationsService } from '../donations.service'
 
 @Component({
   selector: 'app-donations',
@@ -13,35 +14,12 @@ export class DonationsComponent implements OnInit {
 
   showAddDonationForm: Boolean = false;
 
-  // mock list. in reality it would get loaded with db donations (GET all donations)
-  public donationsList: Donation[] = [
-    {
-      did: 1,
-      supplyName: 'Water',
-      quantity: 10,
-      time: {hours: 12, minutes: 12},
-      unit: '8oz bottles'
-    },
-    {
-      did: 2,
-      supplyName: 'Toilet Paper',
-      quantity: 5,
-      time: {hours: 12, minutes: 12},
-      unit: 'Rolls'
-    },
-    {
-      did: 3,
-      supplyName: 'Granola Bars',
-      quantity: 8,
-      time: {hours: 12, minutes: 12},
-      unit: 'Boxes of 12'
-    }
-  ]
+  donationsList: Donation[];
 
   location = 'Arecibo'  // only field that comes from user and not donation. (FETCH user.location)
 
   constructor(
-    private formBuilder: FormBuilder,
+    private formBuilder: FormBuilder, private donationsService: DonationsService
   ) {
     this.addDonationForm = this.formBuilder.group({
       supplyName: '',
@@ -51,27 +29,24 @@ export class DonationsComponent implements OnInit {
    }
 
   ngOnInit(): void {
+    this.getDonations();
   }
 
   toggleAddDonation() {
-    console.log("add btn clicked");
     this.showAddDonationForm = !this.showAddDonationForm;
   }
 
   onAddDonationSubmit(donationData) {
     this.addDonationForm.reset();
 
-    // here it is getting pushed to the array to be displayed but also needs to be pushed to db (POST new donation)
-    this.donationsList.push({
-      did: this.donationsList.length + 1,
-      supplyName: donationData.supplyName,
-      quantity: donationData.quantity,
-      time: {hours: 12, minutes: 12},
-      unit: donationData.unit
-    });
+    this.donationsService.addDonation(donationData);
 
     console.warn('donation added: ', donationData);    
 
+  }
+
+  getDonations(): void {
+    this.donationsList = this.donationsService.getDonations();
   }
 
 }
