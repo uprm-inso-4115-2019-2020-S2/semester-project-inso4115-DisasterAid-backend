ceb522f7b86f722633ac142d4f28bcacbb5897b2
Hector Dael <hectordael@Hectors-MacBook-Pro.local>
2020-05-09 12:29:50 -0400

    Fixed db placeholders and added new ones and Supply class

--- disaster-aid/src/app/home/home.component.ts
+++ disaster-aid/src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UserApiService } from '../userapi.service';
 import {Subscription} from 'rxjs/Subscription';
 import { User } from '../user';
+import { Supply } from '../supply';
 
 
 @Component({
@@ -17,20 +18,91 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor(private userApi: UserApiService) { }
   
   //Mock data; in practicality, these will be loaded with the info from db. 
-  user = "John Doe"
+  //user type should be User
+  user = {
+    firstName: "X AE A-12",
+    lastName: "Musk"
+  }
+
   numOfDonations = 3
   numOfRequests = 7
 
   //mock pending list (should be loaded from db)
-  public pendingNamesList: String[] = [
-    "Juan Vargas", "Brian Lopez", "Julia Belinski", "Robert Acar"
-  ]
-  //mock delivered list (should be loaded from db)
-  public deliveredNamesList: String[] = [
-    "Natalie Cruz", "Michael Scott", "Albert Cruz"
+  //List Type should be User
+  pendingList = [
+    {
+      firstName: "Juan",
+      lastName: "Vargas"
+    },
+    {
+      firstName: "Brian",
+      lastName: "Griffin"
+    },
+    {
+      firstName: "Julia",
+      lastName: "Belinski"
+    },
+    {
+      firstName: "Robert",
+      lastName: "Acar"
+    }
   ]
 
+  //mock delivered list (should be loaded from db)
+  //List Type should be User.
+  deliveredList = [
+    {
+      firstName: "Natalie",
+      lastName: "Cruz"
+    },
+    {
+      firstName: "Michael",
+      lastName: "Scott"
+    },
+    {
+      firstName: "Albert",
+      lastName: "Cruz"
+    },
+    {
+      firstName: "John",
+      lastName: "Doe"
+    }
+  ]
   
+  //mock supplies list (to be loaded from db)
+  //List should be of type Supply[]
+  mySuppliesList = [
+    {
+      name: "Water"
+    },
+    {
+      name: "Apple Sauce"
+    },
+    {
+      name: "Blankets"
+    }
+  ]
+
+  //mock resources list (to be loaded from db)
+  //List should be of type Supply[]
+  myResourcesList: Supply[] = [
+    {
+      name: "Water",
+      quantity: 17,
+      location: "Arecibo"
+    },
+    {
+      name: "Canned Beans",
+      quantity: 23,
+      location: "Quebradillas"
+    },
+    {
+      name: "First Aid Kits",
+      quantity: 4,
+      location: "Naguabo"
+    }
+  ]
+
   ngOnInit(): void {
 
     // this.userApi
