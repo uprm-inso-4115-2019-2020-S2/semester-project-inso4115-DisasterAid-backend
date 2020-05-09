dfe17edc6e218e2ace09dc6c331f8b40f3f5abce
luisrampolla <luis.rampolla@upr.edu>
2020-05-08 22:11:35 -0400

    implemented add donation button on donation page

--- disaster-aid/src/app/donations/donations.component.ts
+++ disaster-aid/src/app/donations/donations.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Donation } from '../donation'
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-donations',
@@ -8,15 +9,12 @@ import { Donation } from '../donation'
 })
 export class DonationsComponent implements OnInit {
 
-  mockDonation: Donation = {
-    did: 1,
-    supplyName: 'Water',
-    quantity: 10,
-    time: {hours: 12, minutes: 12},
-    unit: '8oz bottles'
-  }
+  addDonationForm;
 
-  mockDonationsList: Donation[] = [
+  showAddDonationForm: Boolean = false;
+
+  // mock list. in reality it would get loaded with db donations (GET all donations)
+  public donationsList: Donation[] = [
     {
       did: 1,
       supplyName: 'Water',
@@ -40,11 +38,40 @@ export class DonationsComponent implements OnInit {
     }
   ]
 
-  location = 'Arecibo'  // only field that comes from user and not donation
+  location = 'Arecibo'  // only field that comes from user and not donation. (FETCH user.location)
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+    this.addDonationForm = this.formBuilder.group({
+      supplyName: '',
+      quantity: '',
+      unit: '',
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  toggleAddDonation() {
+    console.log("add btn clicked");
+    this.showAddDonationForm = !this.showAddDonationForm;
+  }
+
+  onAddDonationSubmit(donationData) {
+    this.addDonationForm.reset();
+
+    // here it is getting pushed to the array to be displayed but also needs to be pushed to db (POST new donation)
+    this.donationsList.push({
+      did: this.donationsList.length + 1,
+      supplyName: donationData.supplyName,
+      quantity: donationData.quantity,
+      time: {hours: 12, minutes: 12},
+      unit: donationData.unit
+    });
+
+    console.warn('donation added: ', donationData);    
+
   }
 
 }
