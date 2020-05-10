32845aeacc9b186ae3fce4612e5560c5c0afff30
ramon-rosado <33332410+ramon-rosado@users.noreply.github.com>
2020-05-09 23:33:12 -0400

    Updated DB config
    deleted heroku db config

--- config/__init__.py
+++ config/__init__.py
@@ -5,8 +5,6 @@ from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 DEV_DB = 'postgresql://insouser:inso4115@localhost/DisasterAid'
-# LINE BELOW IS FOR WHEN WE WANT TO RUN ON HEROKU
-# DEV_DB = 'postgres://yukoojywyelupb:535a04c9549446512e582543c0d78e6844f1c2363eb646149845abf854592b65@ec2-3-230-106-126.compute-1.amazonaws.com:5432/dc08d0ste8gj9e'
 app = Flask(__name__)
 
 if 'DATABASE_URL' in environ:
