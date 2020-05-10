c7fa1357b181a08b74f19703a7e4c762d08b2dd3
Everson Rodriguez <everson.rodriguez@upr.edu>
2020-05-10 14:33:02 -0400

    Organized app.py

--- app.py
+++ app.py
@@ -12,14 +12,17 @@ from handler.request import RequestHandler
 def index():
     return 'Welcome to Disaster Aid Distribution App!'
 
+
 # USER ENDPOINTS
 @app.route("/DAD/login", methods=['POST'])
 def do_login():
     return UserHandler().do_login(request.json)
 
+
 @app.route("/DAD/logout", methods=['GET'])
 def do_logout():
     return UserHandler().do_logout()
+
 
 @app.route("/DAD/users", methods=['GET', 'POST'])
 def get_all_or_create_users():
@@ -29,6 +32,7 @@ def get_all_or_create_users():
         return UserHandler.create_user(request.json)
     else:
         return jsonify(message="Method not allowed."), 405
+
 
 @app.route('/DAD/users/<int:uid>', methods=['GET', 'PUT', 'DELETE'])
 def get_user_by_id(uid):
@@ -47,6 +51,7 @@ def get_user_by_id(uid):
         return UserHandler.delete_user(uid)
     else:
         return jsonify(message="Method not allowed."), 405
+
 
 # DONATIONS ENDPOINTS
 @app.route('/DAD/donations', methods=['GET', 'POST'])
@@ -68,6 +73,7 @@ def get_all_or_create_donations():
     else:
         return jsonify(message="Method not allowed."), 405
 
+
 @app.route('/DAD/donations/<int:did>', methods=['GET', 'PUT', 'DELETE'])
 def get_donation_by_id(did):
     if request.method == 'GET':
@@ -86,12 +92,14 @@ def get_donation_by_id(did):
     else:
         return jsonify(message="Method not allowed."), 405
 
+
 @app.route('/DAD/donations/user/<int:uid>', methods=['GET'])
 def get_donations_by_user(uid):
     if request.method == 'GET':
         return DonationHandler().get_donations_by_user(uid)
     else:
         return jsonify(message="Method not allowed."), 405
+
 
 # REQUESTS ENDPOINTS
 @app.route("/DAD/requests", methods=['GET', 'POST'])
@@ -102,6 +110,7 @@ def requests():
         return RequestHandler().create_request(request.json)
     else:
         return jsonify(message="Method not allowed."), 405
+
 
 @app.route("/DAD/requests/<int:rid>", methods=['GET', 'PUT', 'DELETE'])
 def request_update(rid):
@@ -114,5 +123,7 @@ def request_update(rid):
     else:
         return jsonify(message="Method not allowed."), 405
 
+
 if __name__ == '__main__':
     app.run()
+
